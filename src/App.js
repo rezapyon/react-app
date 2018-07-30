f3c89697b08ac4b34515087ac792a59cbdad8ebf
rezapyon <juliansyahmreza@gmail.com>
2018-07-30 17:06:18 +0700

    update

--- src/App.js
+++ src/App.js
@@ -25,12 +25,32 @@ class App extends Component {
     });
   }
 
+  groupChangedHandler = (event) => {
+    this.setState({
+        persons: [
+          { name: 'Chaeyoung', age: 19, blood: 'O', group: 'TWICE'},
+          { name: 'Wonyoung', age: 13, blood: 'O', group: event.target.value},
+          { name: 'Yena', age: 19, blood: 'A', group: 'Produce48'}
+        ]
+    });
+  }
+
   render() {
+    const buttonStyle = {
+      backgroundColor: '#fff',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Yo, I'm Chaeyoung's future husband</h1>
         <p>Here is my wife's and her rivals profile : </p>
-        <button onClick={() => this.switchNameHandler('Son Chaeyoung!')}>Switch Name</button>
+        <button
+          style={buttonStyle}
+          onClick={() => this.switchNameHandler('Son Chaeyoung!')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -41,7 +61,8 @@ class App extends Component {
           age={this.state.persons[1].age}
           blood={this.state.persons[1].blood}
           group={this.state.persons[1].group}
-          clickElement={this.switchNameHandler.bind(this, 'Chaeyoung Son')}>
+          clickElement={this.switchNameHandler.bind(this, 'Chaeyoung Son')}
+          changedElement={this.groupChangedHandler}>
             She's the youngest
         </Person>
         <Person 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     const children = (props.children) ? '(' + props.children + ')' : '';
     return (
-        <div>
+        <div className="Person">
             <p> {props.name} is {props.age} years old, she has {props.blood} blood type and she's part of {props.group}. <b onClick={props.clickElement}>{children}</b></p>
+            <input type="text" onChange={props.changedElement} value={props.group}/>
         </div>
     )
 }
