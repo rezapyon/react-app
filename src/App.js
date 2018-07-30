8f7982689a2dca3c54a94154310b06b7b290cff0
rezapyon <juliansyahmreza@gmail.com>
2018-07-30 21:39:45 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,40 +5,45 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      { name: 'Chaeyoung', age: 19, blood: 'O', group: 'TWICE'},
-      { name: 'Wonyoung', age: 13, blood: 'O', group: 'Produce48'},
-      { name: 'Yena', age: 19, blood: 'A', group: 'Produce48'}
+      { id:"W1", name: 'Chaeyoung', age: 19, blood: 'O', group: 'TWICE'},
+      { id:"W2", name: 'Wonyoung', age: 13, blood: 'O', group: 'Produce48'},
+      { id:"W3", name: 'Yena', age: 19, blood: 'A', group: 'Produce48'}
     ],
     otherState: 'Others',
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    // console.log('button clicked');
-    // DON'T DO THIS this.state.person[0].name = 'Son Chaeyoung';
+  groupChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.group = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
 
     this.setState({
-        persons: [
-          { name: newName, age: 19, blood: 'O', group: 'TWICE'},
-          { name: 'Jang Wonyoung', age: 13, blood: 'O', group: 'Produce48'},
-          { name: 'Choi Yena', age: 19, blood: 'A', group: 'Produce48'}
-        ]
+        persons: persons
     });
   }
 
-  groupChangedHandler = (event) => {
-    this.setState({
-        persons: [
-          { name: 'Chaeyoung', age: 19, blood: 'O', group: 'TWICE'},
-          { name: 'Wonyoung', age: 13, blood: 'O', group: event.target.value},
-          { name: 'Yena', age: 19, blood: 'A', group: 'Produce48'}
-        ]
-    });
-  }
-
-  tooglePersonsHandler = () => {
+  togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice(); [ALTERNATIF]
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
   }
 
   render() {
@@ -48,7 +53,26 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
-    }
+    };
+
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((girl, index) => {
+            return <Person 
+              key={girl.id}
+              clickElement={() => this.deletePersonHandler(index)}
+              name={girl.name}
+              age={girl.age}
+              blood={girl.blood}
+              group={girl.group}
+              changedElement={(event) => this.groupChangedHandler(event, girl.id)}/>
+          })}
+        </div>
+      );
+    };
 
     return (
       <div className="App">
@@ -56,31 +80,8 @@ class App extends Component {
         <p>Here is my wife's and her rivals profile : </p>
         <button
           style={buttonStyle}
-          onClick={this.tooglePersonsHandler}>Show Name</button>
-          { 
-            this.state.showPersons === true ? 
-            <div>
-              <Person 
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}
-                blood={this.state.persons[0].blood}
-                group={this.state.persons[0].group}/>
-              <Person 
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                blood={this.state.persons[1].blood}
-                group={this.state.persons[1].group}
-                clickElement={this.switchNameHandler.bind(this, 'Chaeyoung Son')}
-                changedElement={this.groupChangedHandler}>
-                  She's the youngest
-              </Person>
-              <Person 
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}
-                blood={this.state.persons[2].blood}
-                group={this.state.persons[2].group}/>
-            </div> : null
-          }
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {persons}
       </div>
     );
     // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'I\'m Chaeyoung\'s Husband'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,7 @@ const person = (props) => {
     const children = (props.children) ? '(' + props.children + ')' : '';
     return (
         <div className="Person">
-            <p> {props.name} is {props.age} years old, she has {props.blood} blood type and she's part of {props.group}. <b onClick={props.clickElement}>{children}</b></p>
+            <p onClick={props.clickElement}> {props.name} is {props.age} years old, she has {props.blood} blood type and she's part of {props.group}. <b>{children}</b></p>
             <input type="text" onChange={props.changedElement} value={props.group}/>
         </div>
     )
