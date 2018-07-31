48171475f09e2d3f9aef2080c216336544bda2de
rezapyon <juliansyahmreza@gmail.com>
2018-07-31 17:01:56 +0700

    update

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// const persons = (props) => (
+//     props.persons.map((girl, index) => {
+//         return <Person 
+//           key={girl.id}
+//           clickElement={() => this.deletePersonHandler(index)}
+//           name={girl.name}
+//           age={girl.age}
+//           blood={girl.blood}
+//           group={girl.group}
+//           changedElement={(event) => this.groupChangedHandler(event, girl.id)}/>
+//           // <ErrorBoundary key={girl.id}>
+//           // </ErrorBoundary>
+//     })
+// );
+
+// export default persons;
